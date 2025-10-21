719ee461d825b93dc1a0705a506a0220bf27ca24
goldenbull <goldenbull@gmail.com>
2025-10-21 13:58:58 +0800

    add CMakeLists.txt and a custom cpp

---
+++ custom.cpp
@@ -0,0 +1,7 @@
+#include "custom.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ custom.h
@@ -0,0 +1,8 @@
+#ifndef INC_7ZIP_CMAKE_LIBRARY_H
+#define INC_7ZIP_CMAKE_LIBRARY_H
+
+extern "C" {
+void hello();
+}
+
+#endif // INC_7ZIP_CMAKE_LIBRARY_H
